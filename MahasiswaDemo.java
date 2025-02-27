2a871c51dad9d084466a5689080bb5c6865e1f43
daffaputra09 <daffaputra863@gmail.com>
2025-02-27 21:43:10 +0700

    feat: jawaban pertanyaan

--- MahasiswaDemo.java
+++ MahasiswaDemo.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class MahasiswaDemo {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         Mahasiswa[] arrayOfMahasiswa = new Mahasiswa[3];
-        String dummy;
 
         for (int i = 0; i < 3; i++) {
             arrayOfMahasiswa[i] = new Mahasiswa();
@@ -18,18 +16,13 @@ public class MahasiswaDemo {
             System.out.print("Kelas : ");
             arrayOfMahasiswa[i].kelas = sc.nextLine();
             System.out.print("IPK   : ");
-            dummy = sc.nextLine();
-            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(sc.nextLine());
             System.out.println("-----------------------------------------");
         }
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
-            System.out.println("-----------------------------------------");
+            arrayOfMahasiswa[i].cetakInfo(); 
         }
-    } 
+    }
 }
